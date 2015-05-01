d6bc79490fb571ec8bd0da79994fe34c49aae859
constroy <constroy@163.com>
2015-05-01 23:41:57 +0800

    increased screen_fps from 40 to 60

--- src/config.hpp
+++ src/config.hpp
@@ -1,11 +1,17 @@
 #ifndef _CONFIG_H
 #define _CONFIG_H
 
-//Screen
+//Control
+const int time_slot=25;
+
+//Display
 const int screen_width=800;
 const int screen_height=640;
 const int screen_bpp=32;
-const int screen_fps=40;
+const int screen_fps=60;
+const unsigned bar_back_color=0;
+const unsigned bar_front_color=0XFF00;
+const unsigned char bar_alpha=0X7F;
 
 //Item
 enum ItemMask{BULLET=2,TANK=4,SHIP=8};
@@ -14,9 +20,9 @@ const int dy[]={-1,+1,0,0};
 
 //Tank
 const int reload_time=16;
-const unsigned bar_back_color=0;
-const unsigned bar_front_color=0XFF00;
-const unsigned char bar_alpha=0X7F;
+const int bx[]={+18,+18,-4,+44};
+const int by[]={-4,+44,+18,+18};
+
 //Terrain
 const int map_size=32;
 
