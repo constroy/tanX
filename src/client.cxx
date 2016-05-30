07f6ae1d3eb1149393e1afa317e6a58a8a5ed3fa
constroy <constroy.li@gmail.com>
2016-05-31 01:21:40 +0800

    add client support

---
+++ src/client.cxx
@@ -0,0 +1,42 @@
+#include "client.hxx"
+
+#include <cstdio>
+
+Client::Client(const char ip[])
+{
+	sock=socket(AF_INET,SOCK_DGRAM,IPPROTO_UDP);
+	len=sizeof si_srv;
+	memset(&si_srv,0,len);
+	si_srv.sin_family=AF_INET;
+	si_srv.sin_port=htons(6666);
+	inet_aton(ip,&si_srv.sin_addr);
+}
+void Client::Connect(int &id,int &num)
+{
+	buf[0]=0;
+	buf[1]=0;
+	sendto(sock,buf,sizeof(buf),0,(sockaddr *)&si_srv,len);
+	recvfrom(sock,buf,sizeof(buf),0,(sockaddr *)&si_srv,&len);
+	id=buf[0];
+	num=buf[1];
+}
+bool Client::Send(int id,int cmd)
+{
+	buf[0]=id;
+	buf[1]=cmd;
+	return sendto(sock,buf,sizeof(buf),0,(sockaddr *)&si_srv,len)==sizeof(buf);
+}
+bool Client::Receive(int &id,int &cmd)
+{
+	int t=recvfrom(sock,buf,sizeof(buf),MSG_DONTWAIT,(sockaddr *)&si_srv,&len);
+	if (t==sizeof buf)
+	{
+		id=buf[0];
+		cmd=buf[1];
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
---
+++ src/client.hxx
@@ -0,0 +1,22 @@
+#ifndef _CLIENT_HXX
+#define _CLIENT_HXX
+
+#include <cstring>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+class Client
+{
+	private:
+		int sock;
+		int buf[2];
+		unsigned len;
+		sockaddr_in si_srv;
+	public:
+		Client(const char ip_addr[]);
+		void Connect(int &id,int &num);
+		bool Send(int id,int cmd);
+		bool Receive(int &id,int &cmd);
+};
+
+#endif
