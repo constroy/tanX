40ea027c8806ba58b472a6efabb88bc5088e8369
constroy <constroy.li@gmail.com>
2016-09-26 00:37:28 +0800

    reimplement the singleton

--- src/terrain.hxx
+++ src/terrain.hxx
@@ -8,18 +8,20 @@
 
 class Terrain {
 public:
-	static Terrain *GetInstance() {
+	Terrain(Terrain &) = delete;
+	Terrain(Terrain &&) = delete;
+	static Terrain &GetInstance() {
 		static Terrain instance;
-		return &instance;
+		return instance;
 	}
-	void LoadMap();
-	bool Hit(const Item &it);
-	void Destroy(const Item &it);
-	char GetGrid(int x, int y) const {
+	char GetGrid(int x, int y) const {	
 		return map[x][y];
 	}
+	bool Hit(const Item &it);
+	void LoadMap();
 private:
-	Terrain() {}
+	Terrain() = default;
+	~Terrain() = default;
 	char map[map_size][map_size];
 };
 
