437d3a771fd4f40caf71bf56ff509dba66a7e77b
constroy <constroy.li@gmail.com>
2016-09-26 00:37:35 +0800

    use c++14 multithreading to replace SDL_Thread add mutex for tanks and bullets add Show() to run the display cycle (a thread) rewrite the game logic fix a bug: hit any key can cause a shot fix a bug: tank may hit itself when firing

--- src/game.cxx
+++ src/game.cxx
@@ -1,21 +1,30 @@
+#include <mutex>
+#include <thread>
+
 #include <SDL/SDL_mixer.h>
 #include <SDL/SDL_thread.h>
 
+#include "bullet.hxx"
 #include "client.hxx"
 #include "config.hxx"
 #include "display.hxx"
-#include "model.hxx"
+#include "tank.hxx"
+#include "terrain.hxx"
 #include "timer.hxx"
+
+using std::list;
+using std::mutex;
+
+bool quit;
 
 Mix_Music *bgm = nullptr;
 
-Display *display = nullptr;
+mutex item_mutex;
 
-Model model;
-
-list<Tank> &tanks = model.tanks;
-list<Bullet> &bullets = model.bullets;
-Terrain *&terrain = model.terrain;
+list<Tank> tanks;
+list<Bullet> bullets;
+Terrain &terrain = Terrain::GetInstance();
+Display &display = Display::GetInstance();
 Client *client;
 
 int myid, id, cmd, num;
@@ -29,14 +38,10 @@ bool Init() {
 	SDL_WM_SetIcon(SDL_LoadBMP("img/icon.bmp"), nullptr);
 	// Initialize SDL_mixer
 	if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 1024) == -1) return false;
-	// Get an instance of terrain
-	terrain = Terrain::GetInstance();
-	// Get an instance of display
-	display = Display::GetInstance();
 	// Init the display
-	display->Init();
+	display.Init();
 	// Load the map
-	terrain->LoadMap();
+	terrain.LoadMap();
 	// Load the background music
 	bgm = Mix_LoadMUS("snd/tank_draft_mix.mp3");
 	if (bgm == nullptr) return false;
@@ -44,32 +49,33 @@ bool Init() {
 }
 void CleanUp() {
 	// Quit display
-	display->Quit();
+	display.Quit();
 	// Free the music
 	Mix_FreeMusic(bgm);
 	// Quit SDL_mixer
 	Mix_CloseAudio();
+	// Quit SDL
 	SDL_Quit();
 }
-int Show(void *exit) {
-	display->Show(model, (bool *)exit);
-	return 0;
-}
-int Ctrl(void *exit) {
+
+void Ctrl(bool &quit) {
 	Timer timer;
 	SDL_Event event;
-	while (!*(bool *)exit) {
+	while (!quit) {
 		timer.Start();
 		while (SDL_PollEvent(&event)) {
 			if (event.type == SDL_QUIT) {
-				*(bool *)exit = true;
-			} else if (event.type == SDL_KEYDOWN) {
+				quit = true;
+				return;
+			}
+			if (event.type == SDL_KEYDOWN) {
 				switch (event.key.keysym.sym) {
 					case SDLK_UP: cmd = 1; break;
 					case SDLK_DOWN: cmd = 2; break;
 					case SDLK_LEFT: cmd = 3; break;
 					case SDLK_RIGHT: cmd = 4; break;
 					case SDLK_SPACE: cmd = 0; break;
+					default: continue;
 				}
 				client->Send(myid, cmd);
 			} else if (event.type == SDL_KEYUP) {
@@ -78,6 +84,7 @@ int Ctrl(void *exit) {
 					case SDLK_DOWN: cmd = -2; break;
 					case SDLK_LEFT: cmd = -3; break;
 					case SDLK_RIGHT: cmd = -4; break;
+					default: continue;
 				}
 				client->Send(myid, cmd);
 			}
@@ -85,38 +92,51 @@ int Ctrl(void *exit) {
 		if (timer.GetTicks() < time_slot) SDL_Delay(time_slot - timer.GetTicks());
 	}
 }
-bool Check(const Item &a, const Item &b) {
-	if (a.GetX() >= b.GetX() + b.GetW()) return false;
-	if (a.GetX() + a.GetW() <= b.GetX()) return false;
-	if (a.GetY() >= b.GetY() + b.GetH()) return false;
-	if (a.GetY() + a.GetH() <= b.GetY()) return false;
-	return true;
+
+void Show(const bool &quit) {
+	using std::list;
+	Timer timer;
+	Timer fps;
+	unsigned frame = 0;
+	fps.Start();
+	while (!quit) {
+		timer.Start();
+		display.Show(tanks, bullets, terrain, item_mutex);
+		++frame;
+		if (timer.GetTicks() * screen_fps < 1000)
+			SDL_Delay(1000 / screen_fps - timer.GetTicks());
+		if (fps.GetTicks() >= 1000) {
+			printf("fps:%.3f\n", frame * 1000. / fps.GetTicks());
+			frame = 0;
+			fps.Start();
+		}
+	}
 }
+
 int main(int argc, char *argv[]) {
 	if (argc != 2) {
 		puts("usage: tanX server_ip");
 		return -1;
 	}
+	
 	puts("Connecting to server...");
 	client = new Client(argv[1]);
 	client->Connect(myid, num);
 	puts("Server connected!");
 	printf("myid: %d, num: %d", myid, num);
+	
 	if (!Init()) return -1;
-	// Start drwing
-	bool exit = false;
-	SDL_Thread *ctrl_t = SDL_CreateThread(Ctrl, &exit);
-	SDL_Thread *draw_t = SDL_CreateThread(Show, &exit);
-	// Play BGM
-	if (!Mix_PlayingMusic()) if (Mix_PlayMusic(bgm, -1) == -1) return -1;
-
 	for (int i = 1; i <= num; ++i)
 		tanks.push_back(Tank(i, i - 1, i * 4, 24));
-	list<Tank>::iterator tank = tanks.begin();
-
+	
+	std::thread t_ctrl(Ctrl, std::ref(quit));
+	std::thread t_show(Show, std::ref(quit));
+	// Play BGM
+	if (!Mix_PlayingMusic()) if (Mix_PlayMusic(bgm, -1) == -1) return -1;
 	Timer timer;
-	while (!exit) {
+	while (!quit) {
 		timer.Start();
+		item_mutex.lock();
 		while (client->Receive(id, cmd)) {
 			for (auto &t : tanks) {
 				if (t.GetId() == id) {
@@ -125,42 +145,44 @@ int main(int argc, char *argv[]) {
 				}
 			}
 		}
-		for (auto i = tanks.begin(), j = tanks.begin(); i != tanks.end(); i = j) {
+		for (auto i = tanks.begin(), j = i; i != tanks.end(); i = j) {
 			++j;
 			if (i->Dead()) tanks.erase(i);
+			if (i->Reload()) bullets.push_back(i->Fire());
 			i->Move(+1);
-			if (terrain->Hit(*i)) {
+			if (terrain.Hit(*i)) {
 				i->Move(-1);
-			} else {
-				for (auto k = tanks.begin(); k != tanks.end(); ++k) {
-					if (i != k && Check(*i, *k)) i->Move(-1);
+				continue;
+			}
+			for (const auto &k : tanks)
+				if (i->GetId() != k.GetId() && Check(*i, k)) {
+					i->Move(-1);
+					break;
 				}
-			}
-
-			if (i->Reload()) {
-				bullets.emplace_back(0, i->GetX() + 18, i->GetY() + 18,
-									 i->GetDir(), i->GetPow());
-			}
 		}
 		for (auto i = bullets.begin(), j = i; i != bullets.end(); i = j) {
 			++j;
 			i->Move(+1);
-			if (terrain->Hit(*i)) bullets.erase(i);
-			else {
-				for (auto k = tanks.begin(); k != tanks.end(); ++k) {
-					if (Check(*i, *k)) {
-						k->Damage(i->GetPow());
-						bullets.erase(i);
-					}
+			if (terrain.Hit(*i)) {
+				bullets.erase(i);
+				continue;
+			}
+			for (auto &&k : tanks) {
+				if (Check(*i, k)) {
+					k.Damage(i->GetPow());
+					bullets.erase(i);
+					break;
 				}
 			}
 		}
-		if (timer.GetTicks() < time_slot) SDL_Delay(time_slot - timer.GetTicks());
+		item_mutex.unlock();
+		if (timer.GetTicks() < time_slot)
+			SDL_Delay(time_slot - timer.GetTicks());
 	}
 	Mix_HaltMusic();
-	exit = true;
-	SDL_WaitThread(ctrl_t, nullptr);
-	SDL_WaitThread(draw_t, nullptr);
+	t_ctrl.join();
+	t_show.join();
 	CleanUp();
+	delete client;
 	return 0;
 }
